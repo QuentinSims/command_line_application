bf7caedf92ca8f8b890252cc7c34e70434d943fe
Quentin Sims <quentin@arksoft.co.za>
2024-12-31 20:05:12 +0200

    ability for user to add file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::io;
 mod file_utils;
 
 fn main() {
-    let file_path = r#"c:\Users\QuentinSims\Documents\rustreadtest.txt"#;
+    // let file_path = r#"c:\Users\QuentinSims\Documents\rustreadtest.txt"#;
+
+    println!("Enter the file path:");
+    let mut file_path = String::new();
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect("Failed to read input");
+
+    let file_path = file_path.trim();
 
     match file_utils::process_file(file_path) {
         Ok(contents) => println!("First line: {}", contents),
